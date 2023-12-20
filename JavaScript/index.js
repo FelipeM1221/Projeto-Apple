651d19d8859cb8da8dfe642737af893406f879b6
FelipeM1221 <fm448999@gmail.com>
2023-12-20 09:42:44 -0300

    Finalizei o projeto

---
+++ JavaScript/index.js
@@ -0,0 +1,11 @@
+function clicar(){
+    var div_navegaçao = window.document.getElementById('navegaçao')
+
+    if (div_navegaçao.style.display == 'none'){
+        div_navegaçao.style.display = 'block'
+    }
+
+    else{
+        div_navegaçao.style.display = 'none'
+    }
+}
